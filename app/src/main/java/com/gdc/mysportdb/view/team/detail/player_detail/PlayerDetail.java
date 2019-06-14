89365612c9836617ddf3266b703935c5d96ce96a
reksa <karang.reksa@gdc.co.id>
2019-06-14 11:25:47 +0700

    Finishing Player Detail

--- app/src/main/java/com/gdc/mysportdb/view/team/detail/player_detail/PlayerDetail.java
+++ app/src/main/java/com/gdc/mysportdb/view/team/detail/player_detail/PlayerDetail.java
@@ -2,7 +2,9 @@ package com.gdc.mysportdb.view.team.detail.player_detail;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.gdc.mysportdb.R;
 import com.gdc.mysportdb.data.model.Player;
@@ -11,6 +13,7 @@ import com.squareup.picasso.Picasso;
 public class PlayerDetail extends AppCompatActivity {
 
     private ImageView imgPoster;
+    private TextView tvWeight, tvHeight, tvPosition, tvDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +21,38 @@ public class PlayerDetail extends AppCompatActivity {
         setContentView(R.layout.activity_player_detail);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        imgPoster = findViewById(R.id.img_playerPoster);
+        initView();
 
+        initListener();
+    }
+
+    private void initListener() {
         Player playerName = getIntent().getParcelableExtra("KEY_PLAYER");
         setTitle(playerName.getStrPlayer());
         Picasso.get().load(playerName.getPoster()).placeholder(R.drawable.placeholder).into(imgPoster);
 
+        if (TextUtils.isEmpty(playerName.getStrWeight())) {
+            tvWeight.setText("");
+        } else if (TextUtils.isEmpty(playerName.getStrHeight())) {
+            tvHeight.setText("");
+        } else if (TextUtils.isEmpty(playerName.getStrWeight()) && TextUtils.isEmpty(playerName.getStrHeight())) {
+            tvWeight.setText("");
+            tvHeight.setText("");
+        } else {
+            tvWeight.setText(playerName.getStrWeight());
+            tvHeight.setText(playerName.getStrHeight());
+        }
+
+        tvPosition.setText(playerName.getStrPosition());
+        tvDescription.setText(playerName.getStrDescriptionEN());
+    }
+
+    private void initView() {
+        imgPoster = findViewById(R.id.img_playerPoster);
+        tvWeight = findViewById(R.id.tv_weight);
+        tvHeight = findViewById(R.id.tv_height);
+        tvPosition = findViewById(R.id.tv_position);
+        tvDescription = findViewById(R.id.tv_description);
     }
 
     @Override
